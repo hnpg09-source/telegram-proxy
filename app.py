88cf66a2fbbc1b3a630414655ce74affdad654a6
hnpg09-source <hnpg09@gmail.com>
2026-05-03 19:30:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ CHAT_ID = "6131465835"
 @app.route('/send')
 def send():
     texto = request.args.get('text', 'sin mensaje')
+    texto = texto.replace('+', ' ')
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     r = requests.post(url, json={"chat_id": CHAT_ID, "text": texto})
     return r.text
@@ -18,4 +20,5 @@ def index():
     return "Proxy activo"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
